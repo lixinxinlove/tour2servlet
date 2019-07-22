04f5f5c5f6d9ab31a01fe904afa0290a53d48a4d
lixinxin <895330766@qq.com>
2019-07-22 15:56:28 +0800

    完善页面

--- src/main/java/com/lixinxinlove/web/servlet/RouteServlet.java
+++ src/main/java/com/lixinxinlove/web/servlet/RouteServlet.java
@@ -1,7 +1,6 @@
 package com.lixinxinlove.web.servlet;
 
 
-
 import com.lixinxinlove.entity.PageEntity;
 import com.lixinxinlove.entity.Route;
 import com.lixinxinlove.entity.User;
@@ -21,8 +20,10 @@ public class RouteServlet extends BaseServlet {
 
     private RouteService routeService = new RouteServiceImpl();
     private FavoriteService favoriteService = new FavoriteServiceImpl();
+
     /**
      * 分页查询
+     *
      * @param request
      * @param response
      * @throws ServletException
@@ -37,37 +38,43 @@ public class RouteServlet extends BaseServlet {
 
         //接受rname 线路名称
         String rname = request.getParameter("rname");
-        rname = new String(rname.getBytes("iso-8859-1"),"utf-8");
+        if (rname==null) {
+            rname = "";
+        } else {
+
+        }
+        rname = new String(rname.getBytes("iso-8859-1"), "utf-8");
 
 
         int cid = 0;//类别id
         //2.处理参数
-        if(cidStr != null && cidStr.length() > 0 && !"null".equals(cidStr)){
+        if (cidStr != null && cidStr.length() > 0 && !"null".equals(cidStr)) {
             cid = Integer.parseInt(cidStr);
         }
         int currentPage = 0;//当前页码，如果不传递，则默认为第一页
-        if(currentPageStr != null && currentPageStr.length() > 0){
+        if (currentPageStr != null && currentPageStr.length() > 0) {
             currentPage = Integer.parseInt(currentPageStr);
-        }else{
+        } else {
             currentPage = 1;
         }
 
         int pageSize = 0;//每页显示条数，如果不传递，默认每页显示5条记录
-        if(pageSizeStr != null && pageSizeStr.length() > 0){
+        if (pageSizeStr != null && pageSizeStr.length() > 0) {
             pageSize = Integer.parseInt(pageSizeStr);
-        }else{
+        } else {
             pageSize = 5;
         }
 
         //3. 调用service查询PageBean对象
-        PageEntity<Route> pb = routeService.pageQuery(cid, currentPage, pageSize,rname);
+        PageEntity<Route> pb = routeService.pageQuery(cid, currentPage, pageSize, rname);
         //4. 将pageBean对象序列化为json，返回
-        writeValue(pb,response);
+        writeValue(pb, response);
 
     }
 
     /**
      * 根据id查询一个旅游线路的详细信息
+     *
      * @param request
      * @param response
      * @throws ServletException
@@ -81,11 +88,12 @@ public class RouteServlet extends BaseServlet {
         //2.调用service查询route对象
         Route route = routeService.findOne(rid);
         //3.转为json写回客户端
-        writeValue(route,response);
+        writeValue(route, response);
     }
 
     /**
      * 判断当前登录用户是否收藏过该线路
+     *
      * @param request
      * @param response
      * @throws ServletException
@@ -98,10 +106,10 @@ public class RouteServlet extends BaseServlet {
         //2. 获取当前登录的用户 user
         User user = (User) request.getSession().getAttribute("user");
         int uid;//用户id
-        if(user == null){
+        if (user == null) {
             //用户尚未登录
             uid = 0;
-        }else{
+        } else {
             //用户已经登录
             uid = user.getUid();
         }
@@ -110,11 +118,12 @@ public class RouteServlet extends BaseServlet {
         boolean flag = favoriteService.isFavorite(rid, uid);
 
         //4. 写回客户端
-        writeValue(flag,response);
+        writeValue(flag, response);
     }
 
     /**
      * 添加收藏
+     *
      * @param request
      * @param response
      * @throws ServletException
@@ -126,19 +135,19 @@ public class RouteServlet extends BaseServlet {
         //2. 获取当前登录的用户
         User user = (User) request.getSession().getAttribute("user");
         int uid;//用户id
-        if(user == null){
+        if (user == null) {
             //用户尚未登录
-            return ;
-        }else{
+            return;
+        } else {
             //用户已经登录
             uid = user.getUid();
         }
 
 
         //3. 调用service添加
-        favoriteService.add(rid,uid);
+        favoriteService.add(rid, uid);
 
     }
 
 
-    }
+}
